cb453e274a3865d52380d857a50ba5a31772e8a1
AsOne2020 <13816531+AsOne2048@user.noreply.gitee.com>
2024-05-16 01:08:57 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -9,12 +9,22 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'AsOne的Blog',
   subtitle: '主页',
-  lang: 'zh_CN',
-  themeHue: 250,
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  themeColor: {
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
+  },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',
+    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ]
 }
 
 export const navBarConfig: NavBarConfig = {
